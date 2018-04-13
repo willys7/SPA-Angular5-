4753ec23b6dc6592c3f478b87ca713ba55091ddf
willys7 <wilfuentes0320@gmail.com>
2018-04-13 10:00:55 -0600

    add routerlink in the component

--- src/app/components/heroes/heroes.component.ts
+++ src/app/components/heroes/heroes.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { HeroesService, Heroe } from '../../services/heroes.service'
+import { Router } from "@angular/router";
 
 @Component({
   selector: 'app-heroes',
@@ -10,12 +11,17 @@ export class HeroesComponent implements OnInit {
 
   heroes:Heroe[] = [];
   
-  constructor( private _heroesService: HeroesService) { 
+  constructor( private _heroesService: HeroesService, 
+                private router:Router) { 
 
   }
 
   ngOnInit() {
     this.heroes = this._heroesService.getHeroes();
+  }
+
+  showHeroe(index:number){
+    this.router.navigate(['/heroe',index])
   }
 
 }
